201df63533d9b7f35dfb6a1f99f547be0ca82ac1
aditpurohit <asp2215@columbia.edu>
2019-12-09 01:26:49 +0000

    Models added

--- squirrel_tracker/settings.py
+++ squirrel_tracker/settings.py
@@ -38,8 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'squirrel_tracker_app',
-    
+    'crispy_forms'
 ]
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
